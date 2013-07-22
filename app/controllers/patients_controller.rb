74bb3f25793d34617ef2b14435fcb48a5879fa25
Yinghao Lei <leiyinghao.bj@gmail.com>
2013-07-22 13:33:39 +0800

    修改患者控制器

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -24,14 +24,15 @@ class PatientsController < ApplicationController
   # POST /patients
   # POST /patients.json
   def create
+
     @patient = Patient.new(patient_params)
 
     respond_to do |format|
       if @patient.save
-        format.html { redirect_to @patient, notice: 'Patient was successfully created.' }
+        #format.html { redirect_to @patient, notice: 'Patient was successfully created.' }
         format.json { render action: 'show', status: :created, location: @patient }
       else
-        format.html { render action: 'new' }
+        #format.html { render action: 'new' }
         format.json { render json: @patient.errors, status: :unprocessable_entity }
       end
     end
