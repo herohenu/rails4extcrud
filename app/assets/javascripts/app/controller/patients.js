1932d5a94703f74c86246e3f62dc8c2e0640fb8a
Yinghao Lei <leiyinghao.bj@gmail.com>
2013-07-22 13:33:19 +0800

    添加测试函数  弹出window  updateorSave

--- app/assets/javascripts/app/controller/patients.js
+++ app/assets/javascripts/app/controller/patients.js
@@ -17,13 +17,19 @@
                   render: this.onPanelRendered
                   },*/
                  'patientlist': {
-                     itemdblclick: this.addPatient
+                     itemdblclick: this.updatePatient
 
                  },
-                 'patientedit button[action=save]': {
-                     click: this.updatePatient
+                 ' patientadd  button[action=save]': {
+//                     click: this.updatePatient
+                     click: this.createOrUpdateUser
                  }
 
+
+/*                 ,
+                 'button[action=addPatient]': {
+                     click: this.addUser
+                 }*/
 
 
              });
@@ -33,24 +39,22 @@
           console.log('The panel was rendered');
           }*/
 
-         editPatient: function(grid, record) {
+/*         editPatient: function(grid, record) {
            //  console.log('Double clicked on ' + record.get('name'));
              var view = Ext.widget('patientedit');
              view.down('form').loadRecord(record);
 
 
-         },
+         },*/
 
 
          addPatient: function() {
-             //  console.log('Double clicked on ' + record.get('name'));
              var view = Ext.widget('patientadd');
-             //view.down('form').loadRecord(record);
-              view.show();
+             view.show();
 
          },
 
-         updatePatient: function(button) {
+/*         updatePatient: function(button) {
              // console.log('clicked the Save button');
              var win    = button.up('window'),
                  form   = win.down('form'),
@@ -59,5 +63,75 @@
 
              record.set(values);
              win.close();
+         },*/
+
+/*         deletePatient: function (){
+           var  grid  = this.getPatientlist(),
+                record = grid.getSelectionModel().getSelection(),
+                store  = this.getPatientsStore;
+
+                store.remove(record),
+                    this.getPatientsStore.sync();
+
+
+         },
+         */
+
+         createOrUpdateUser:function(button){
+             console.log("点击了保存按钮");
+
+            //  alert(1);
+           var win = button.up('window');
+             var form = win.down('form');
+
+             //var store = this.getUsersStore();
+             var store = this.getPatientsStore();
+             var values = form.getValues();
+
+//             var user = Ext.create('AM.model.User', values);
+             var patient = Ext.create('AM.model.Patient', values);
+
+
+
+
+             var errors = patient.validate();
+
+             if (errors.isValid()) {
+                 var formRecord = form.getRecord();
+
+                 if (formRecord) {
+                     // perform update
+                     console.log(" form record set values ");
+                     //la
+
+                     formRecord.set(values);
+                 } else {
+                     // perform create
+                     //store.add(user);
+                     store.add(patient);
+                 }
+
+                 store.sync({
+                     success: function() {
+                         win.close();
+                     },
+                     failure: function(batch, options) {
+                         // extract server side validation errors
+                         var serverSideValidationErrors = batch.exceptions[0].error;
+
+                         var errors = new Ext.data.Errors();
+                         for (var field in serverSideValidationErrors) {
+                             var message = serverSideValidationErrors[field].join(", ");
+                             errors.add(undefined, { field: field, message: message });
+                         }
+                         form.getForm().markInvalid(errors);
+                     }
+                 });
+             } else {
+                 form.getForm().markInvalid(errors);
+             }
          }
+
+
+
      });
